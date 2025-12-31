b3fd09ad3252a2a3a8d0c9e97ea8e3c375b736c8
ujangdoubleday <domba88galing@gmail.com>
2025-12-31 13:28:36 +0700

    chore(config): remove dotenv configuration files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 
-// Load dotenv if available (optional dependency) - suppress output
-try {
-  const dotenv = await import('dotenv');
-  dotenv.config({ debug: false });
-} catch (e) {
-  // dotenv not installed, ignore
-}
-
 import { program, analyzeAction } from './cli/index.js';
 
 program.action(analyzeAction);
